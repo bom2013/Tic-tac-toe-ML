3d506c7a1ab35e8f8f4a1e7cea1f6ca79d376cd1
bom2013 <noam21288@gmail.com>
2020-04-06 21:42:30 +0300

    Fix ts problem
    Fix the problem in the train set generator

--- critic.py
+++ critic.py
@@ -1,12 +1,28 @@
 import perf_sys
+
+
+#train - [board, v_train, player]
 def make_train_set(m, w, starter):
     train_list = []
     cur_p = starter
+    game_res = perf_sys.is_end_of_game(m[-1]) #Should be either 1 or 2 or a draw (3)
     for b in m:
-        if b  == m[-1]:
-            train_list.append([b, 100, cur_p])
+        if b == m[-2]:#he lose\draw, cant win
+            if game_res == 3:#draw
+                train_list.append([b, 0, cur_p])
+            else: #he lose
+                train_list.append([b, -100, cur_p])
+        elif b  == m[-1]:#he win\draw
+            if game_res == 3:#draw
+                train_list.append([b, 0, cur_p])
+            else:
+                train_list.append([b, 100, cur_p])
         else:
             train_list.append([b, perf_sys.calc_v(w, m[m.index(b)+1], cur_p), cur_p])
         cur_p = perf_sys.p_tag(cur_p)
     return train_list
 
+def player_after_n_moves(starter, n):
+    if n%2 == 0:
+        return starter
+    return perf_sys.p_tag(starter)
--- manager.py
+++ manager.py
@@ -6,18 +6,20 @@ from random import uniform
 #w = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 #print(critic.make_train_set(perf_sys.play_game_against_himself([[0,0,0],[0,0,0],[0,0,0]], 1, w), w, 1))
 
-NUM_OF_TEST =10
+NUM_OF_TEST =1000000
 w=[]
 for i in range(31):
     w.append(uniform(-5.0, 5.0))
 print(w)
 for i in range(NUM_OF_TEST):
     s_b = exp_gen.get_exp(1)
-    m = perf_sys.play_game_against_himself(s_b, 1, w)
+    m = perf_sys.play_game_against_himself(s_b, 2, w)
+    #print("m: "+str(m))
     ts = critic.make_train_set(m, w, 1)
+    #print("ts: "+str(ts))
     w = generalizer.LMS_update(ts, w)
-    print(w)
-    print("---------")
+    #print(w)
+    #print("---------")
     if i%100 == 0:
         print("do: "+ str(100*(i/NUM_OF_TEST)) + "%")
 print("Done!")
