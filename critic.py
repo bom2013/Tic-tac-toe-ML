4df00031cce8d34c11e9dbefe7e11aa62bea95e0
bom2013 <noam21288@gmail.com>
2020-04-26 20:40:30 +0300

    Update critic.py
    update critic and add comments

--- critic.py
+++ critic.py
@@ -1,11 +1,15 @@
-import perf_sys
+'''
+This module create train examples from game
+'''
+import performance_system
 
 
 #train - [board, v_train, player]
+#This function create train set(set of train examples)
 def make_train_set(m, w, starter):
     train_list = []
     cur_p = starter
-    game_res = perf_sys.is_end_of_game(m[-1]) #Should be either 1 or 2 or a draw (3)
+    game_res = performance_system.is_end_of_game(m[-1]) #Should be either 1 or 2 or a draw (3)
     for b in m:
         if b == m[-2]:#he lose\draw, cant win
             if game_res == 3:#draw
@@ -18,11 +22,12 @@ def make_train_set(m, w, starter):
             else:
                 train_list.append([b, 100, cur_p])
         else:
-            train_list.append([b, perf_sys.calc_v(w, m[m.index(b)+1], cur_p), cur_p])
-        cur_p = perf_sys.p_tag(cur_p)
+            train_list.append([b, performance_system.calc_v(w, m[m.index(b)+1], cur_p), cur_p])
+        cur_p = performance_system.p_tag(cur_p)
     return train_list
 
+#This function return who is the player after n moves from start
 def player_after_n_moves(starter, n):
     if n%2 == 0:
         return starter
-    return perf_sys.p_tag(starter)
+    return performance_system.p_tag(starter)
