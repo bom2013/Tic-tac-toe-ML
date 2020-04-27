a04c88ea2b34c7dabde5f76c077a6a05cc697970
bom2013 <noam21288@gmail.com>
2020-04-27 18:33:09 +0300

    Update experiment_generator.py
    add comments

--- experiment_generator.py
+++ experiment_generator.py
@@ -1,10 +1,9 @@
 import random
 
-#This function create "experiment"(start board)
+# This function create "experiment"(start board)
 def get_exp(starter):
-    b = [[0,0,0],[0,0,0],[0,0,0]]
+    b = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     x = random.randrange(3)
     y = random.randrange(3)
     b[x][y] = starter
     return b
-
