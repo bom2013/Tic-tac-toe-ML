ffc24116337f98938e00f1b7fbd72a857eda9e5b
bom2013 <noam21288@gmail.com>
2020-04-27 21:54:47 +0300

    Create basic manager
    add learn option, need to add the play option

--- manager.py
+++ manager.py
@@ -1,28 +1,52 @@
 import critic
-import perf_sys
-import exp_gen
+import performance_system
+import experiment_generator
 import generalizer
 from random import uniform
-#w = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-#print(critic.make_train_set(perf_sys.play_game_against_himself([[0,0,0],[0,0,0],[0,0,0]], 1, w), w, 1))
 
-NUM_OF_TEST =250000
-w=[]
-for i in range(31):
-    w.append(uniform(-5.0, 5.0))
-print(w)
-with open("w_start.txt", "w")as f:
-    f.write(str(w))
-for i in range(NUM_OF_TEST):
-    s_b = exp_gen.get_exp(1)
-    m = perf_sys.play_game_against_himself(s_b, 2, w)
-    ts = critic.make_train_set(m, w, 1)
-    w = generalizer.LMS_update(ts, w)
-    if i%100 == 0:
-        print("do: "+ str(100*(i/NUM_OF_TEST)) + "%")
-print("Done!")
-print(w)
+'''
+This module used for train management and study with the other modules
+'''
+end_main = False
+while not end_main:
+    print("--------------")
+    print("Welcome to manager view")
+    print("1. Create and train new weight")
+    print("2. Play game using weight")
+    print("3. Exit")
+    inp = int(input('Enter your choice: '))
+    if inp == 1:
+        num_of_trains = int(input('Enter number of trains: '))
+        print("Creates a starting vector with random values")
+        w = []
+        for i in range(31):
+            w.append(uniform(-5.0, 5.0))
+        print("Starting vector is: ")
+        print(w)
+        if input("Do you want to save start vector in file before running the learning process(y\\n)? ") == "y":
+            with open(input("Enter file name: ")+".txt", "w")as f:
+                f.write(str(w))
+            print("File saved")
+        print("Start lerning process")
+        for i in range(num_of_trains):
+            s_b = experiment_generator.get_exp(1)
+            m = performance_system.play_game_against_himself(s_b, 2, w)
+            ts = critic.make_train_set(m, w, 1)
+            w = generalizer.LMS_update(ts, w)
+            if i % 100 == 0:
+                print("update: " + str(100*(i/num_of_trains)) + "%")
+        print("Done!")
+        print(w)
+        if input("Do you want to save the vector(y\n)? ") == "y":
+            with open(input("Enter file name: ")+".txt", "w")as f:
+                f.write(str(w))
+            print("File saved")
+    elif inp == 2:
+        pass
+    else:
+        exit()
+
 #input("enter to play against comp")
-#perf_sys.play_game_against_user(1,w)
-with open("w_learned.txt", "w")as f:
-    f.write(str(w))
+# perf_sys.play_game_against_user(1,w)
+# with open("w_learned.txt", "w")as f:
+ #   f.write(str(w))
